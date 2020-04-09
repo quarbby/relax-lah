afdfcc5ddf23b1ca1c768c335612a2ebcdb1ddff
quarbby <lynnette-ng@outlook.com>
2020-04-09 12:31:54 +0800

    worry text

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,12 @@
 function smileyBtnClicked(req, res, body) {
+    // get current date
+    // get date/time smiley clicked
+    // get start end smiley
+
     $.ajax({
         url: "/",
         method: "POST",
-        data: { id: "hello"},
+        data: { smiley_number: "1", is_start_smiley: true, is_end_smiley: true},
       }).done(function(response) {
         //window.location.href = '/worry';
         // dont kill me
@@ -10,4 +14,8 @@ function smileyBtnClicked(req, res, body) {
       }).fail(function( error ) {
         console.log(error);
       });
+}
+
+function worryFormSubmit() {
+    var worryText = $('#worry-form').elements['worry-text-input'];
 }
