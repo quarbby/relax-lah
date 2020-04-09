da9cfb93fa3594eb90d075ea462ba861c9584060
quarbby <lynnette-ng@outlook.com>
2020-04-09 09:42:35 +0800

    added smiles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/catalog');
-});
+var page_controller = require('../controllers/pageController');
+
+router.get('/', page_controller.index);
 
 module.exports = router;
